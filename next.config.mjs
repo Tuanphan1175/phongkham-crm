f37077909db93d3cdc4028ce3363b0579ca07e98
tuanphan1175 <tulovy2007@gmail.com>
2026-03-16 15:25:39 +0700

    fix: upgrade to Next.js 15 + React 19 to fix route group manifest ENOENT bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["bcryptjs", "bcrypt"],
-  },
+  serverExternalPackages: ["bcryptjs", "bcrypt"],
 };
 
 export default nextConfig;
